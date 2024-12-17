833be4a817892d14e0d896c5a72b23d8a1b311f7
Jon <jonmontefer@gmail.com>
2024-12-17 10:54:15 +0100

    Primer commit

---
+++ src/clasesDatos/Empleado.java
@@ -0,0 +1,10 @@
+package clasesDatos;
+
+public class Empleado {
+	private String DNI;
+	private String Nombre;
+	private String Apellidos;
+	private String Categoria;
+	private boolean ClubSocial; //Indica si el empleado esta en el club social de la empresa
+	private float sueldo;
+}
