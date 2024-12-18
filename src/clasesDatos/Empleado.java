50fd8159922b2facf0c095c755caae6ab1dd2afa
Jon <jonmontefer@gmail.com>
2024-12-18 09:22:43 +0100

    Añadido toString y otros cambios

--- src/clasesDatos/Empleado.java
+++ src/clasesDatos/Empleado.java
@@ -7,11 +7,7 @@ public class Empleado {
 	private String Categoria;
 	private boolean ClubSocial; //Indica si el empleado esta en el club social de la empresa
 	private float sueldo;
-	
-	
-	
 	/**
-	 * Constructor Completo
 	 * @param dNI
 	 * @param nombre
 	 * @param apellidos
@@ -21,73 +17,66 @@ public class Empleado {
 	 */
 	public Empleado(String dNI, String nombre, String apellidos, String categoria, boolean clubSocial, float sueldo) {
 		super();
-		this.DNI = dNI;
-		this.Nombre = nombre;
-		this.Apellidos = apellidos;
-		this.Categoria = categoria;
-		this.ClubSocial = clubSocial;
+		DNI = dNI;
+		Nombre = nombre;
+		Apellidos = apellidos;
+		Categoria = categoria;
+		ClubSocial = clubSocial;
 		this.sueldo = sueldo;
 	}
-	
-	//Constructor vacio
-	public Empleado{
-		super();
-		this.DNI = "";
-		this.Nombre = "**********";
-		this.Apellidos = "************";
-		this.Categoria = "Base";
-		this.ClubSocial = false;
-		this.sueldo = (float)0.0;
+	public Empleado(String dNI, String nombre, String apellidos) {
+		// TODO Auto-generated constructor stub
 	}
-
 	public String getDNI() {
 		return DNI;
 	}
-
 	public void setDNI(String dNI) {
 		DNI = dNI;
 	}
-
 	public String getNombre() {
 		return Nombre;
 	}
-
 	public void setNombre(String nombre) {
 		Nombre = nombre;
 	}
-
 	public String getApellidos() {
 		return Apellidos;
 	}
-
 	public void setApellidos(String apellidos) {
 		Apellidos = apellidos;
 	}
-
 	public String getCategoria() {
 		return Categoria;
 	}
-
 	public void setCategoria(String categoria) {
 		Categoria = categoria;
 	}
-
 	public boolean isClubSocial() {
 		return ClubSocial;
 	}
-
 	public void setClubSocial(boolean clubSocial) {
 		ClubSocial = clubSocial;
 	}
-
+	
 	public float getSueldo() {
 		return sueldo;
 	}
-
 	public void setSueldo(float sueldo) {
 		this.sueldo = sueldo;
 	}
-	
+	public void setClubSocial() {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public String toString() {
+		return "Empleado [DNI=" + DNI + ", Nombre=" + Nombre + ", Apellidos=" + Apellidos + ", Categoria=" + Categoria
+				+ ", ClubSocial=" + ClubSocial + ", sueldo=" + sueldo + "]";
+	}
 }
-
-
+	
+	
+	
+	
+	
+	
